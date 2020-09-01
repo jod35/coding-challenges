b7e873592d2303a43d60c3bfe87b0848c0f93b1e
jona <jodestrevin@gmail.com>
2020-09-01 12:11:13 +0300

    008

---
+++ 008.py
@@ -0,0 +1,6 @@
+total_bill=int(input("Enter the total bill "))
+number_of_diners=int(input("Enter the number of diners: "))
+
+amount=total_bill/number_of_diners
+
+print("Each diner is supposed to pay ,",amount)
