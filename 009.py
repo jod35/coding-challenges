9549f4bcded47fe99213c8661d7d94ffd3871c39
jona <jodestrevin@gmail.com>
2020-09-01 12:20:47 +0300

    009

---
+++ 009.py
@@ -0,0 +1,7 @@
+number_of_days=int(input("Enter the number of days: "))
+
+hours=number_of_days*24
+minutes=number_of_days*24*60
+seconds=number_of_days*24*60*60
+
+print(f"In {number_of_days} days, there are {hours} hours, {minutes} minutes and {seconds} seconds.")
