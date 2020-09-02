e449cf7f8ac56c4e4c09c261eaef2fbd4245af30
jona <jodestrevin@gmail.com>
2020-09-02 09:41:05 +0300

    017

---
+++ 017.py
@@ -0,0 +1,11 @@
+age =int(input("Enter your age: "))
+
+if age >= 18:
+    print("You can vote")
+elif age == 17:
+    print("You can learn how to drive")
+elif age == 16:
+    print("You can buy a lottery ticket")
+else:
+    print("You can go trick or treating")
+
