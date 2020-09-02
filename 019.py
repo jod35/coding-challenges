536d0216a3b0e4ef3202932a5acf6dcbf8325b65
jona <jodestrevin@gmail.com>
2020-09-02 15:52:34 +0300

    019

---
+++ 019.py
@@ -0,0 +1,10 @@
+number=int(input("Enter a number: "))
+
+if number == 1:
+    print("Thank you")
+elif number == 2:
+    print("Well done")
+elif number == 3:
+    print("Correct")
+else:
+    print("Error message")
