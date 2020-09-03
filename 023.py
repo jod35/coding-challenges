f6ebd9d983f8e0195d8ba5aaaaede38cc4fbbddb
jona <jodestrevin@gmail.com>
2020-09-03 22:23:13 +0300

    023

---
+++ 023.py
@@ -0,0 +1,10 @@
+rhyme_line=input("Enter a line of your fav nursery rhyme: ")
+
+print("The length of the line is ",len(rhyme_line))
+
+first_range=int(input("Enter your first range: "))
+
+last_range=int(input("Enter your last range: "))
+
+print(rhyme_line[first_range:last_range])
+
