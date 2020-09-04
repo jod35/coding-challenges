cdd407037afb34ad0521ed425d45e6bb1b950589
jona <jodestrevin@gmail.com>
2020-09-04 20:19:35 +0300

    031

---
+++ 031.py
@@ -0,0 +1,8 @@
+import math
+
+radius=float(input("Enter a radius: "))
+
+area=math.pi *(radius*radius)
+
+print("The area of the circle with radius {} is {:.2f} square units1".format(radius,area))
+
