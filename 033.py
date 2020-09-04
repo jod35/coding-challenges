74e6180f43f0d3eb5eff02b7ac6f43d20bf5bbf2
jona <jodestrevin@gmail.com>
2020-09-04 20:46:29 +0300

    033

---
+++ 033.py
@@ -0,0 +1,10 @@
+first_number=float(input("Enter a first number: "))
+
+second_number=float(input("Enter a second number: "))
+
+quotient=first_number/second_number
+
+remainder=first_number%second_number
+
+print("{} ÷ {} = {} rem {}".format(first_number,second_number,quotient,remainder))
+
