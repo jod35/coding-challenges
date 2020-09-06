3970dd2c34c3ea1dd4998928686eb44c8d787ddf
jona <jodestrevin@gmail.com>
2020-09-06 19:09:35 +0300

    042

---
+++ 042.py
@@ -0,0 +1,8 @@
+total =0
+
+for i in range(0,5):
+    number=int(input("Enter a number: "))
+    option=int(input("Enter 1 to add yototal or any other number to skip: "))
+    if option ==1:
+        total+=option
+print(total)
