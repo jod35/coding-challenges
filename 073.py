d5aa3f83d14805817fd1e94b3fcf086a467be0a3
jona <jodestrevin@gmail.com>
2020-09-11 11:23:19 +0300

    073

---
+++ 073.py
@@ -0,0 +1,8 @@
+word=input("Enter your your favorite word from your favorite poem: ")
+
+start_point=int(input("Enter the start point: "))
+
+end_point=int(input("Enter the end point: "))
+
+print(word[start_point:end_point])
+
