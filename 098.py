dfac523bb33720c9300ccc03c75b25d92b319a65
jona <jodestrevin@gmail.com>
2020-09-16 22:54:50 +0300

    098

--- 098.py
+++ 098.py
@@ -2,12 +2,22 @@ print("1) Create a file\n2) Display a file\n3) Add a new item to the file\nEnter
 
 option=int(input(">>>> "))
 
+
 if option == 1:
-    pass
+    file_=open("Subjects.txt","w")
+    subject=input("Enter a subject: ")
+
+    file_.write(subject)
+    file_.close()
 elif option == 2:
-    pass
+    file_=open("Subjects.txt","r")
+    print(file_.read())
 elif option == 3:
-    pass
+    file_=open("Subjects.txt","a")
+    subject=input("Enter a subject: ")
+
+    file_.write(subject)
+    file_.close()
 else:
     print("Please enter 1,2 or 3")
 
