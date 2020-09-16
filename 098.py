e36f7a0222e040f06e3ddb7238c6e448688921d4
jona <jodestrevin@gmail.com>
2020-09-16 22:42:57 +0300

    097

--- 098.py
+++ 098.py
@@ -1,1 +1,13 @@
-print("1) Create a file\n2) Display a file\n3) Add a new item to the file\nEnter options 1,2,3
+print("1) Create a file\n2) Display a file\n3) Add a new item to the file\nEnter options 1,2,3")
+
+option=int(input(">>>> "))
+
+if option == 1:
+    pass
+elif option == 2:
+    pass
+elif option == 3:
+    pass
+else:
+    print("Please enter 1,2 or 3")
+
